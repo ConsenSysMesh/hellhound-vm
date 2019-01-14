fc486eab45296368cee6d37df82f464e686dd938
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-01-14 11:05:54 +0100

    Update dispatcher_test.go

--- core/dispatcher_test.go
+++ core/dispatcher_test.go
@@ -3,23 +3,20 @@ package core
 import (
 	"github.com/ConsenSys/hellhound-vm/dispatchers"
 	"github.com/ConsenSys/hellhound-vm/hh"
+	"reflect"
 	"testing"
 )
 
-var (
-	d = NewDispatcher(
+func Test_dispatcher_Dispatch(t *testing.T) {
+	d := NewDispatcher(
 		dispatchers.Keystore(),
-		dispatchers.RegisterSet(),
 	)
-)
-
-func TestDispatcher_Dispatch(t *testing.T) {
-
 	var tests = []struct {
 		name    string
 		opcode  hh.OpCode
 		wantErr bool
 	}{
+		{"nominal case", hh.OpCode(hh.LOADKEY), false},
 		{"0xFF unknown opcode", hh.OpCode(0xFF), true},
 	}
 
@@ -35,4 +32,67 @@ func TestDispatcher_Dispatch(t *testing.T) {
 		})
 	}
 
+}
+
+func TestDispatcher(t *testing.T) {
+	tests := []struct {
+		name string
+		want TandenConfigurer
+	}{
+		{
+			name: "nominal case",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := Dispatcher()
+			if d == nil{
+				t.Error("dispatcher should not be nil")
+			}
+		})
+	}
+}
+
+func TestDispatcherCfg(t *testing.T) {
+	type args struct {
+		subDispatchers [][]hh.Operation
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "nominal case",
+			args: args{subDispatchers: [][]hh.Operation{dispatchers.Arithmetic()}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			configurer := DispatcherCfg(tt.args.subDispatchers...)
+			if configurer == nil {
+				t.Error("configurer should not be nil")
+			}
+			configurer(&tanden{})
+		})
+	}
+}
+
+func TestNewDispatcher(t *testing.T) {
+	type args struct {
+		subDispatchers [][]hh.Operation
+	}
+	tests := []struct {
+		name string
+		args args
+		want hh.Dispatcher
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewDispatcher(tt.args.subDispatchers...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewDispatcher() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
